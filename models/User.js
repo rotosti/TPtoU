bf7a1f88167f9de0f0e5dd55e1b7b6b7d1d6aaea
Xavier Martinez <xavier@Xaviers-MacBook-Air.local>
2021-05-11 11:34:58 -0500

    added b crypt

--- models/User.js
+++ models/User.js
@@ -1,7 +1,12 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
-class User extends Model { }
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+      }
+ }
 
 User.init(
     {
@@ -32,7 +37,13 @@ User.init(
             allowNull: false,
         }
     },
-    {
+    
+    {  hooks: {
+        beforeCreate: async (newUserData) => {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+        },
+    },
         sequelize,
         freezeTableName: true,
         timestamps: false,
